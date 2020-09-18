79e6f37b6093f368931e5af2c116c1752b6ff306
BREEZ-IO <66539673+BREEZ-IO@users.noreply.github.com>
2020-09-17 20:53:32 -0500

    Update Plants.java

--- Plants.java
+++ Plants.java
@@ -2,7 +2,28 @@ import java.util.*;
 public class PlantsDictionary {
 
   public static void Main(String [args]) {
-  
+    
+  }
+
+    public static void PlantFinder(String searchPlant){
+      Scanner scanner = new Scanner(file);
+      File plants = new File("plant_data.txt");
+      
+      while (scanner.hasNextLine()) {
+        String line = scanner.nextLine();
+        if line.contains(searchPlant) {
+          //Get the next lines that contain plant info and
+          //add them to whichever data structure we choose
+          //then return it.
+        
+      }
+      
+    }
+    
+    
+    
+    
+      
   }
 
 }
