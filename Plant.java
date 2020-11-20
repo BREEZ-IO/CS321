bc326ef3b0e15e1b0b0bdc798df45c49415f7684
laihipp <71467721+laihipp@users.noreply.github.com>
2020-11-19 21:11:52 -0600

    Update Plant.java

--- Plant.java
+++ Plant.java
@@ -10,21 +10,59 @@ import java.util.Date;
  * @author amberlaihipp
  */
 public class Plant {
-    public Plant(String name, String sciName, String nickname, String light, String water, String mLevel) {
+    public Plant(String name, String sciName, String nickname, String light, String water, String lastWatered, String mLevel) {
         plantName = name;
         plantSciName = sciName;
         plantNickname = nickname;
         plantLight = light;
         plantWaterInfo = water;
+        lastWatered = lastWatered;
         maintenanceLevel = mLevel;
+    }
+    
+    public String getName() {
+        return plantName;
+    }
+    
+    public String getSciName() {
+        return plantSciName;
+    }
+    
+    public String getNickname() {
+        return plantNickname;
+    }
+    
+    public void setNickname(String nickname) {
+        this.plantNickname = nickname;
+    }
+    
+    public String getLightInfo() {
+        return plantLight;
+    }
+    
+    public String getWaterInfo() {
+        return plantWaterInfo;
+    }
+    
+    public String getLastWaterDate() {
+       return lastWatered;
+    }
+    
+    //public void setLastWaterDate(Date d) {
+    //    
+    //}
+    
+    public String getMaintenanceLevel() {
+        return maintenanceLevel;
     }
     
     public void printInfo() {
         System.out.println("Name: " + plantName);
-        System.out.println("SciName: " + plantSciName);
+        System.out.println("Scientific Name: " + plantSciName);
         System.out.println("Nickname: " + plantNickname);
         System.out.println("Light: " + plantLight);
         System.out.println("Water: " + plantWaterInfo);
+        System.out.println("Last Watered: " + lastWatered);
         System.out.println("Maintenance Level: " + maintenanceLevel);
     }
     
@@ -33,7 +71,7 @@ public class Plant {
     private String plantNickname;
     private String plantLight;
     private String plantWaterInfo;
-    private Date lastWatered;
+    private String lastWatered;
     private String maintenanceLevel;
 }
 
