fc8f05f090dd3a67ab9f0ddde6d35474a6b35d5c
laihipp <71467721+laihipp@users.noreply.github.com>
2020-11-19 21:10:52 -0600

    Update Utility.java

--- Utility.java
+++ Utility.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.*;
+import java.util.Iterator;
 /**
  * A class to load files
  * @author amberlaihipp
@@ -14,12 +15,10 @@ public class Utility {
     }
 
     public int loadFile(String fileName, String fileType, ArrayList aList) {           
-          // FileInputStream in = null;
-           //int c;
-
            try {      
                FileReader read = new FileReader(fileName);
                Scanner scanner = new Scanner(read); // create scanner
+               String line = "";
                
                if (fileType.equals("Plants")) {
                    // initialize temp variables
@@ -28,6 +27,7 @@ public class Utility {
                    String tempNickname = "";
                    String tempLight = "";
                    String tempWaterInfo = "";
+                   String tempLastWaterDate = "";
                    String tempMLevel = "";
                                       
                    while (scanner.hasNextLine()) { // While there are more lines in the file 
@@ -36,10 +36,12 @@ public class Utility {
                            tempNickname = scanner.nextLine();
                            tempLight = scanner.nextLine();
                            tempWaterInfo = scanner.nextLine();
+                           tempLastWaterDate = scanner.nextLine();
                            tempMLevel = scanner.nextLine();
                            
+                           line = scanner.nextLine();
                            // Create Plant Object with info
-                           Plant p = new Plant(tempName, tempSciName, tempNickname, tempLight, tempWaterInfo, tempMLevel);
+                           Plant p = new Plant(tempName, tempSciName, tempNickname, tempLight, tempWaterInfo, tempLastWaterDate, tempMLevel);
                            // Add plant to arrayList that was passed in
                            aList.add(p);
                        }
@@ -58,5 +60,35 @@ public class Utility {
            }
 
            return 0;
+    }
+    
+    public boolean writeFile(ArrayList<Plant> list) {
+        try {            
+            FileWriter writer = new FileWriter("MyPlants.txt");
+            
+            for (Plant p : list) {
+                writer.write(p.getName() + "\n");
+                writer.write(p.getSciName() + "\n");
+                writer.write(p.getNickname() + "\n");
+                writer.write(p.getLightInfo() + "\n");
+                writer.write(p.getWaterInfo() + "\n");
+                writer.write(p.getLastWaterDate() + "\n");
+                writer.write(p.getMaintenanceLevel() + "\n");
+                writer.write("***************\n");
+                
+                writer.flush();
+            }
+            
+            writer.close();
+            System.out.println("File saved successfully");
+            
+            return true;           
+        }
+        catch (IOException e) {
+            System.out.println("An ERROR occured saving the file.");
+            e.printStackTrace();
+            
+            return false;
+        }
     }
 }
