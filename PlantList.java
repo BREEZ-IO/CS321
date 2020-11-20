71dd441e3a19159a0c8e11e0256abb87f0341ded
BREEZ-IO <66539673+BREEZ-IO@users.noreply.github.com>
2020-11-19 20:09:04 -0600

    Update PlantList.java

--- PlantList.java
+++ PlantList.java
@@ -1,6 +1,4 @@
 import java.util.ArrayList;
-import java.util.Scanner;
-import java.io.*;
 /**
  * A class to load files
  * @author amberlaihipp
@@ -10,10 +8,31 @@ public class PlantList {
      * 
      */
     public PlantList(String filename) {
-        ArrayList<String> plantsList = new ArrayList<String>();
         Utility test = new Utility();
         test.loadFile(filename, "Plants", plantsList);
     }
     
+    public void AddPlant(Plant newPlant){
+        plantsList.add(newPlant);
+    }
     
+    public void RemovePlant(Plant deadPlant){
+        plantsList.remove(deadPlant);
+    }
+    
+    public void PrintList(){
+        for (int i = 0; i < plantsList.size(); i++) {
+            Plant temp = (plantsList.get(i));
+            System.out.println(temp.plantNickname);
+            System.out.println(temp.plantSciName);
+            System.out.println(temp.plantName);
+            System.out.println(temp.plantLight);
+            System.out.println(temp.plantWaterInfo);
+            System.out.println(temp.lastWatered);
+                    
+        }  
+    }
+    
+    ArrayList<Plant> plantsList = new ArrayList<Plant>();
 }
+
