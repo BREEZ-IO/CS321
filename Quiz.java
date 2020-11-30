ebe32105f6f7bff65d227010998adc7fbc9677f2
laihipp <71467721+laihipp@users.noreply.github.com>
2020-11-30 16:04:20 -0600

    Add files via upload

--- Quiz.java
+++ Quiz.java
@@ -1,35 +1,73 @@
 import java.util.*;
 
+/**
+ * @author Michaela Dent
+ * Quiz holds a question bank, and chooses a plant based on the answers
+ * user gives. 
+ */
 public class Quiz {
-    public static void Main(String[] args){
-
+    
+    private ArrayList<String> questions; //list of questions
+    private String answer; 
+    private PlantList plantOptions; // array list of plants copy 
+    private Plant chosenPlant; //Plant object 
+    private boolean continueQuiz = true; 
+    
+    /**
+     * Default constructor for Quiz class. 
+     * @param 
+     */
+    public Quiz() {
+        // create list of plant options
+        PlantList plantOptions = new PlantList("MainPlants.txt");
     }
-
-    public static List GenerateQuiz(int questions){
-
-
-
-        List answers = null;
-        for (int i = 0; i<questions; i++){
-            userAnswer = CallQuestions(i);
-            answers.add(userAnswer);
+    
+    private void askQuestions() {
+        int i = 0; //number of questions to help stop loop  
+        
+        Scanner a = new Scanner(System.in);
+        while (continueQuiz == true || i <= questions.size()-1) {
+            //read question from arrayList 
+            System.out.println(questions.get(i));
+            
+            //store answer 
+            answer = a.nextLine(); 
+            
+            //compare variable to the factor in other plants 
+            //CALL arrayList.remove choice for for plants that do not have that factor
+            for(int j = plantOptions.size()-1; j >= 0; j--) {//truly inefficient
+                if (plantOptions.get(j).fieldA != answer) {
+                    plantOptions.removePlant(plantOptions.get(j));
+                }
+                else if (plantOptions.get(j).fieldB != answer) {
+                    plantOptions.removePlantOption(plantOptions.get(j));
+                }
+                else if (plantOptions.get(j).fieldC != answer) {
+                    plantOptions.removePlantOption(plantOptions.get(j));
+                }      
+            }
+            
+            //IF there are n > 1 plants, continue quiz ELSE continueQuiz = false
+            if (plants.size() < 1) {
+                continueQuiz = false; 
+            }
+            i ++; 
         }
-        return answers;
-    }
-
-    public static int AnswersToPlant(List answers){
-    
-        return 0;
+        
+        chosenPlant = choosePlant(plantOptions);
     }
     
-    public static String CallQuestions(int i){
-        //Get the question i, ask the user, then return the answer
-        String userAnswer = "";
-        
-        
-        
-        return userAnswer;
+    private void removePlantOption(Plant p) {
+        this.remove(p); 
     }
     
-
+    private Plant choosePlant(PlantList p) {
+        //random num generator 
+        //return index
+        Random rand = new Random(); 
+        int index =  rand.nextInt(p.size());
+        chosenPlant = p.get(index); 
+        return chosenPlant; 
+    }
+    
 }
