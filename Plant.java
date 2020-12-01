8a99be42bb2267d02f4d6a8aeec92d6b0b35a5fa
laihipp <71467721+laihipp@users.noreply.github.com>
2020-11-30 20:45:34 -0600

    Update Plant.java

--- Plant.java
+++ Plant.java
@@ -1,13 +1,9 @@
 import java.util.Date;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
+ * Plant class used to represent an individual plant
  *
- * @author amberlaihipp
+ * @author Amber Lai Hipp
  */
 public class Plant {
     public Plant(String name, String sciName, String nickname, String light, String water, String lastWatered, String mLevel) {
