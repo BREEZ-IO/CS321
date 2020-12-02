667b368a06d3a3cbe72cd21845479964e8fec8ae
laihipp <71467721+laihipp@users.noreply.github.com>
2020-12-01 19:25:20 -0600

    Update Utility.java

--- Utility.java
+++ Utility.java
@@ -15,52 +15,51 @@ public class Utility {
         
     }
 
-    public int loadFile(String fileName, String fileType, ArrayList aList) {           
-           try {      
-               FileReader read = new FileReader(fileName);
-               Scanner scanner = new Scanner(read); // create scanner
-               String line = "";
+    public void loadFile(String fileName, String fileType, ArrayList aList) {
+        try {
+            FileReader read = new FileReader(fileName);
+            Scanner scanner = new Scanner(read); // create scanner
+            String line = "";
                
-               if (fileType.equals("Plants")) {
-                   // initialize temp variables
-                   String tempName = "";
-                   String tempSciName = "";
-                   String tempNickname = "";
-                   String tempLight = "";
-                   String tempWaterInfo = "";
-                   String tempLastWaterDate = "";
-                   String tempMLevel = "";
+            if (fileType.equals("Plants")) {
+                // initialize temp variables
+                String tempName = "";
+                String tempSciName = "";
+                String tempNickname = "";
+                String tempLight = "";
+                String tempWaterInfo = "";
+                String tempLastWaterDate = "";
+                String tempMLevel = "";
                                       
-                   while (scanner.hasNextLine()) { // While there are more lines in the file 
-                           tempName = scanner.nextLine(); // set tempName to line read in
-                           tempSciName = scanner.nextLine();
-                           tempNickname = scanner.nextLine();
-                           tempLight = scanner.nextLine();
-                           tempWaterInfo = scanner.nextLine();
-                           tempLastWaterDate = scanner.nextLine();
-                           tempMLevel = scanner.nextLine();
+                while (scanner.hasNextLine()) { // While there are more lines in the file 
+                    tempName = scanner.nextLine(); // set tempName to line read in
+                    tempSciName = scanner.nextLine();
+                    tempNickname = scanner.nextLine();
+                    tempLight = scanner.nextLine();
+                    tempWaterInfo = scanner.nextLine();
+                    tempLastWaterDate = scanner.nextLine();
+                    tempMLevel = scanner.nextLine();
                            
-                           line = scanner.nextLine();
-                           // Create Plant Object with info
-                           Plant p = new Plant(tempName, tempSciName, tempNickname, tempLight, tempWaterInfo, tempLastWaterDate, tempMLevel);
-                           // Add plant to arrayList that was passed in
-                           aList.add(p);
-                       }
-                   }
-               else if (fileType.equals("Questions")){
-                   while (scanner.hasNextLine()) { // While there are more lines in the fill
-                       aList.add(scanner.nextLine()); // add question to list
-                   }
-               }
+                    line = scanner.nextLine();
+                    // Create Plant Object with info
+                    Plant p = new Plant(tempName, tempSciName, tempNickname, tempLight, tempWaterInfo, tempLastWaterDate, tempMLevel);
+                    // Add plant to arrayList that was passed in
+                    aList.add(p);
+                }
+            }
+            else if (fileType.equals("Questions")){
+                while (scanner.hasNextLine()) { // While there are more lines in the file
+                    line = scanner.nextLine(); // read in the line
+                    aList.add(line); // add question to list
+                }
+            }
                
-               scanner.close(); // close scanner
-           }   
-           catch (Exception FileNotFoundException) {
-               System.out.println("ERROR: File not found.");
-               System.exit(0);
-           }
-
-           return 0;
+           scanner.close(); // close scanner
+        }   
+        catch (Exception FileNotFoundException) {
+            System.out.println("ERROR: File not found.");
+            System.exit(0);
+        }
     }
     
     public boolean writeFile(ArrayList<Plant> list) {
