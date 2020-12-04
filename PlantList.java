9ec316188b14ad67d0836fcecb6b9ebb6b61f4a3
BREEZ-IO <66539673+BREEZ-IO@users.noreply.github.com>
2020-12-03 20:16:25 -0600

    Add copies

--- PlantList.java
+++ PlantList.java
@@ -1,34 +1,42 @@
-import java.util.ArrayList;
+package main;
+
+import java.io.File;
+import java.time.LocalDate;
+import java.util.*;
 
 /**
  * A class to create and manipulate lists of plants.
- * @param plantsList An Arraylist of Plant objects that will hold all plants in the PlantList
+ * 
  * @author Bre McNichols and Amber Lai Hipp and Michaela Dent
  */
 public class PlantList {
     /**
      * Constructor function to create a plant list based on a text file.
-     @param filename: A string containing the address of the file holding all plants to be added
-     @param util: A utility class to use the loadFile function 
+     * 
+     *@param fileName a string containing the address of the file holding all plants to be added.
+     *@param util a utility class to use the loadFile function.
      */    
-    public PlantList(String filename, Utility util) {
-        // if trying to create MyPlants
+    public PlantList(String fileName, Utility util) {
+        // If trying to create MyPlants
         if (fileName.equals("MyPlants.txt")) {
-            // check to see if file exists
+            // Check to see if file exists
             File myPlants = new File(fileName);
-            // if file does not exist
+            // If file does not exist
             if (myPlants.isFile() == false) {
                 // return empty list
                 return;
             }
         }
-        util.loadFile(filename, "Plants", plantsList);
+        
+        util.loadFile(fileName, plantsList);
     }
+    
     /**
-    Function to add a plant to the plant list. It can also add a last watered date if needed.
-    @param newPlant: the plant that is going to be added to the list.
-    @param setLastWateredDate: A boolean that tells if the plant needs a last-watered date or not.
-    */
+     * Function to add a plant to the plant list. It can also add a last watered date if needed.
+     * 
+     * @param newPlant the plant that is going to be added to the list.
+     * @param setLastWateredDate a boolean that tells if the plant needs a last-watered date or not.
+     */
     public void addPlant(Plant newPlant, boolean setLastWateredDate) {
         if (setLastWateredDate == true) {
             newPlant.setLastWateredDate();
@@ -36,18 +44,22 @@ public class PlantList {
         
         plantsList.add(newPlant); 
     }
+    
     /**
-    Function to remove a given plant from a list.
-    @param deadPlant: The specific plant that will be removed from the list.
-    */
+     * Removes a given plant from the list of plants.
+     * 
+     * @param deadPlant the plant to be removed from the list.
+     */
     public void removePlant(Plant deadPlant) {
         plantsList.remove(deadPlant);
     }
+    
     /**
-    Function to return the entire list as a string.
-    @return output Each plant's toString function conjoined together
-    @see toString()
-    */
+     * Function to return the entire list as a string.
+     * 
+     * @return output each plant's toString function conjoined together.
+     * @see toString()
+     */
     public String printList(){
         String output = "";
         for (Plant p : plantsList) {
@@ -56,34 +68,41 @@ public class PlantList {
         
         return output;
     }
+    
     /**
-    Searches through a plantlist for a given plant by its name or nickname
-    @param search The search keyword
-    @param searchType Defines if the plant is being searched by name or nickname
-    @return p The Plant object that was being searched for, or null if not found
-    */
+     * Searches through a plant list for a given plant by its name or nickname.
+     * 
+     * @param search the search keyword.
+     * @param searchType defines if the plant is being searched by name or nickname.
+     * @return p the plant that was being searched for, or null if not found.
+     */
     public Plant searchList(String search, String searchType) {
         for (Plant p : plantsList) {
             if (searchType.equals("Nickname")){
                 if (p.getNickname().contains(search)){
-                    return p;
+                    Plant temp = p.copy();
+                    return temp;
                 }
             } 
             else if (searchType.equals("Plant Name")) {
                 if (p.getName().contains(search)){
-                    return p;
+                    Plant temp = p.copy();
+                    return temp;
                 }
-            )
+            }    
         }
-            
+        
         System.out.println("Plant " + search + " not found.");
         return null;
     }
+    
     /**
-    Removes plants from the list who's attributes don't fit into the user's needs.
-    @param search The search key designating the exact attribute being searched for
-    @param searchType The type of attribute that is being searched for, Water, Maintenance, or Light
-    */
+     * Removes plants from the list whose attributes don't fit into 
+     * the users needs.
+     * 
+     * @param search the search key designating the exact attribute being searched for.
+     * @param searchType the type of attribute that is being searched for, Water, Maintenance, or Light.
+     */
     public void filterList(String search, String searchType) {
         ArrayList<Plant> mismatch = new ArrayList<Plant>();
         for (Plant p : plantsList) {
@@ -104,7 +123,7 @@ public class PlantList {
                     continue;
             }
             else if (searchType.equals("Light")) {
-                if (p.getLightInfo().contains(search))
+                if (p.getLightInfo().equals(search))
                     continue;
                 else {
                     mismatch.add(p);
@@ -112,36 +131,59 @@ public class PlantList {
             }
         }
         // remove mismatches
-        plantsList.removeAll(mismatch);       
-        // add some sort of exception check for empty list
+        plantsList.removeAll(mismatch);
+        // add some sort of exception check for empty list?
         return;
     }
+    
     /**
-    Function to save the list as a text file that can be read in the next time the program is opened.
-    */
+     * Saves the list as a text file.
+     */
     public void saveList() {
-        Utility test = new Utility();
-        test.writeFile(plantsList);
+        Utility util = new Utility();
+        util.writeFile(plantsList);
     }
+    
     /**
-    Returns the length of the list in plants.
-    @return plantsList.size() the size of the plantList
-    @see plantsList.size()
-    */
+     * Gives the length of the list of plants.
+     * @return size of plant list as an integer. 
+     */
     public int getSize() {
         return plantsList.size();
     }
+    
     /**
-    Function to get a random plant out of a list
-    @return plantsList.get(i or 0) The randomized Plant that will be returned
-    */
+     * Allows an outside class to iterate through the plantsList by returning 
+     *  the plant at a given index.
+     * 
+     * @param index the index of the plant you want to get access to.
+     * @return a plant object.
+     */
+    public Plant iterateList(int index) {
+        try {
+            Plant temp = plantsList.get(index).clone();
+            return temp;
+        }
+        catch (Error ArrayIndexOutOfBoundsException) {
+            System.out.println("Error accessing plant in list0");
+            System.exit(0);
+        }
+        return null;
+    }
+
+    /**
+     * Picks a random plant from the list of plants.
+     * 
+     * @return the plant object that was selected.
+     */
     public Plant getRandom() {
         if (plantsList.size() == 1)
-            return plantsList.get(0);
+            Plant temp = plantsList.get(0).clone();
+            return temp;
         Random rand = new Random();
         int i = rand.nextInt(plantsList.size());
-        
-        return plantsList.get(i);
+        Plant temp = plantsList.get(i).clone();
+        return temp;
     }
     
     private ArrayList<Plant> plantsList = new ArrayList<Plant>();
